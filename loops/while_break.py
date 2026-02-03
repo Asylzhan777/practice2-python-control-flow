c5b6c9918923ddcb14383c1130541c593757007e
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:11:57 +0600

    Update while_break.py

--- loops/while_break.py
+++ loops/while_break.py
@@ -5,3 +5,35 @@ while True:
     if i == 3:
         break
     i += 1
+
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+
+nums = [2, 4, 6, 8, 10]
+i = 0
+
+while i < len(nums):
+    if nums[i] == 6:
+        print("Нашли 6!")
+        break
+    i += 1
+
+while True:
+    password = input("Введите пароль: ")
+    if password == "1234":
+        print("Доступ разрешён")
+        break
+
+nums = [5, 7, 3, -2, 9]
+i = 0
+
+while i < len(nums):
+    if nums[i] < 0:
+        break
+    print(nums[i])
+    i += 1
+
