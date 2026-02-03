5f0d4f28a631f748cf1bca1d5961ee5a73c1df15
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:07:45 +0600

    Update for_break.py

--- loops/for_break.py
+++ loops/for_break.py
@@ -3,3 +3,29 @@ for i in range(1, 6):
     if i == 4:
         break
     print(i)
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+numbers = [1, 3, 7, 9, 11]
+
+for n in numbers:
+    if n == 7:
+        print("Нашли 7!")
+        break
+
+nums = [4, 6, 2, -1, 8]
+
+for n in nums:
+    if n < 0:
+        break
+    print(n)
+
+passwords = ["1234", "qwerty", "admin", "secret"]
+
+for p in passwords:
+    if p == "admin":
+        print("Доступ запрещён")
+        break
