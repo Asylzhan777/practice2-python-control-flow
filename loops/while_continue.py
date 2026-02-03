40e0979c47abcdec127f8af530bfc330fcc09103
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:13:13 +0600

    Update while_continue.py

--- loops/while_continue.py
+++ loops/while_continue.py
@@ -5,3 +5,37 @@ while i < 5:
     if i == 3:
         continue
     print(i)
+
+i = 0
+while i < 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i)
+
+i = 0
+while i < 10:
+    i += 1
+    if i % 2 == 0:
+        continue
+    print(i)
+
+nums = [3, -2, 6, -1, 9]
+i = 0
+
+while i < len(nums):
+    if nums[i] < 0:
+        i += 1
+        continue
+    print(nums[i])
+    i += 1
+
+words = ["hi", "ok", "python", "a", "code"]
+i = 0
+
+while i < len(words):
+    if len(words[i]) < 3:
+        i += 1
+        continue
+    print(words[i])
+    i += 1
