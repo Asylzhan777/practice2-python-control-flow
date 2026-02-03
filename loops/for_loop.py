9664527780927a22443d1b773267807107e757bc
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:09:57 +0600

    Update for_loop.py

--- loops/for_loop.py
+++ loops/for_loop.py
@@ -1,3 +1,23 @@
 # Пример 4: for loop
 for i in range(1, 6):
     print(i)
+
+for i in range(1, 7):
+    print(i)
+
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+nums = [1, 2, 3, 4]
+
+for n in nums:
+    print(n * 2)
+
+total = 0
+
+for i in range(1, 6):
+    total += i
+
+print(total)
