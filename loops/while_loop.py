3cec858f76bc35abcfbff49825e6d6279fbb9f08
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:14:37 +0600

    Update while_loop.py

--- loops/while_loop.py
+++ loops/while_loop.py
@@ -3,3 +3,31 @@ i = 1
 while i <= 5:
     print(i)
     i += 1
+
+i = 1
+while i <= 7:
+    print(i)
+    i += 1
+
+fruits = ["apple", "banana", "orange"]
+i = 0
+
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+
+i = 1
+total = 0
+
+while i <= 5:
+    total += i
+    i += 1
+
+print(total)
+
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
+
+
