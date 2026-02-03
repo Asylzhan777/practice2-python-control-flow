bb60caed1bdbb194b7c8163836bc8b31f7c42e21
Asylzhan777 <asylzhanbzhn@gmail.com>
2026-02-03 12:08:46 +0600

    Update for_continue.py

--- loops/for_continue.py
+++ loops/for_continue.py
@@ -3,3 +3,27 @@ for i in range(1, 6):
     if i == 3:
         continue
     print(i)
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+nums = [3, -1, 5, -7, 9]
+
+for n in nums:
+    if n < 0:
+        continue
+    print(n)
+
+words = ["hi", "hello", "a", "python", "ok"]
+
+for w in words:
+    if len(w) < 3:
+        continue
+    print(w)
